f8e872731c59d10afe0c4d8ffaec310c6456a650
SanjeebLama <sanjeeb.lama004@gmail.com>
2022-11-10 20:20:57 +0545

    Add nodemon

--- api/app.js
+++ api/app.js
@@ -8,12 +8,12 @@ app.use(cors())
 app.get('/', (req, res) => {
   res.json([
     {
-      "id":"10",
+      "id":"Rule 101",
       "title":"Here and Now; Breathe and Relax"
     },
     {
       "id":"2",
-      "title":"Game Review: Pokemon Brillian Diamond"
+      "title":"Yay !!!!!! It's working. Volume is working"
     },
     {
       "id":"3",
